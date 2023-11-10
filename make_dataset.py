a232135ac821a05f765f1f10b9c2590f7930c873
am_mahmoudi <am.mahmoudi@outlook.com>
2023-11-11 02:34:45 +0330

    add make_dataset class
    this class make prblems from data and save them as pkl files

---
+++ make_dataset.py
@@ -0,0 +1,47 @@
+import numpy as np
+import pickle
+import torch
+
+import sys
+import os
+sys.path.insert(1, os.path.join(sys.path[0], os.pardir, os.pardir))
+from utils import T2FProblem
+from BioDataParser import Stoichiometry
+
+data=Stoichiometry("./Data/A.txt","./Data/b.txt","./Data/lb.txt","./Data/ub.txt","./Data/projector.npy")
+
+A=data.get_a_matrix()
+X=data.get_b_vector()
+Y_min=data.get_lb()
+Y_max=data.get_ub()
+
+import os
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
+
+torch.set_default_dtype(torch.float64)
+
+
+num_var = Y_max.shape[1]
+num_ineq = 2*A.shape[0]
+num_eq = A.shape[0]
+num_examples = X.shape[0]
+
+
+# num_ineq = 50
+# for num_eq in [10, 30, 50, 70, 90]:
+#     print(num_ineq, num_eq)
+#     np.random.seed(17)
+#     Q = np.diag(np.random.random(num_var))
+#     p = np.random.random(num_var)
+#     A = np.random.normal(loc=0, scale=1., size=(num_eq, num_var))
+#     X = np.random.uniform(-1, 1, size=(num_examples, num_eq))
+#     G = np.random.normal(loc=0, scale=1., size=(num_ineq, num_var))
+#     h = np.sum(np.abs(G@np.linalg.pinv(A)), axis=1)
+
+problem = T2FProblem(A,Y_min,Y_max,X)
+problem.calc_Y()
+print(len(problem.Y))
+
+with open("./Data/recon2.2_dataset_var{}_ineq{}_eq{}_ex{}".format(num_var, num_ineq, num_eq, num_examples), 'wb') as f:
+    pickle.dump(problem, f)
+
