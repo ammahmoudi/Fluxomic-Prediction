b7fad45dbd2beddec5330722b867cf2272e2c228
am_mahmoudi <am.mahmoudi@outlook.com>
2023-12-17 09:35:21 -0800

    adding prblem based on standard QP notations as simpleprblem 2

--- utils.py
+++ utils.py
@@ -353,4 +353,308 @@ class T2FProblem:
         self._Y = torch.tensor(Y[feas_mask])
         return Y
 
+###################################################################
+# SIMPLE PROBLEM
+###################################################################
+
+class SimpleProblem2:
+    """ 
+        minimize y
+        s.t.        AY=X
+                    Y_min < Y < Y_max   
+
+        num_r= Number of reactions
+        num_m= numer of metabloites
+        num_ineq= 2*num_r
+        num_m=num_eq 
+        X=(num_m, num_examples)
+        A=(num_m,num_r)
+        Y,Y_min,Y_max=(num_r,num_examples )  
+
+        p=(num_r,num_examples)
+        Q=(1,1)
+        G=(2,1)
+        h=(2,num_r)
+        y=v
+        p=-c
+        Q=0
+        Q=(r,1)
+        G=stackup[1,-1]
+        h=stackup[y_up,-y_min]
+        A=S
+        Gy<=h
+        2*1 * 1*r =2*r 
+        minimize_y 1/2 * y^T Q y + p^Ty
+        r*1 1*1 1*r + r*1  1*r
+        s.t.       Ay =  x
+                   Gy <= h
+
+
+
+        y=(r,1)
+        x*r r,1 <= x,1
+        2r,r * r,1 <= 2r,1
+    """
+    def __init__(self, Q, p, A, G, h, X, valid_frac=0.0833, test_frac=0.0833):
+        self._Q = torch.tensor(Q)
+        self._p = torch.tensor(p)
+        self._A = torch.tensor(A)
+        self._G = torch.tensor(G)
+        self._h = torch.tensor(h)
+        self._X = torch.tensor(X)
+        self._Y = None
+        self._xdim = X.shape[1]
+        self._ydim = Q.shape[0]
+        self._num = X.shape[0]
+        self._neq = A.shape[0]
+        self._nineq = G.shape[0]
+        self._nknowns = 0
+        self._valid_frac = valid_frac
+        self._test_frac = test_frac
+        det = 0
+        i = 0
+        # print(self._ydim)
+        # print(self._neq)
+        while abs(det) < 0.0001 and i < 1000:
+            self._partial_vars = np.random.choice(self._ydim, self._ydim - self._neq, replace=False)
+            self._other_vars = np.setdiff1d( np.arange(self._ydim), self._partial_vars)
+            det = torch.det(self._A[:, self._other_vars])
+            logger.info("det of iteration "+str(i)+" is: "+str(det))
+            i += 1
+        if i == 1000:
+            raise Exception
+        else:
+            self._A_partial = self._A[:, self._partial_vars]
+            self._A_other_inv = torch.inverse(self._A[:, self._other_vars])
+
+        ### For Pytorch
+        self._device = None
+
+    def __str__(self):
+        return 'SimpleProblem2-{}-{}-{}-{}'.format(
+            str(self.ydim), str(self.nineq), str(self.neq), str(self.num)
+        )
+
+    @property
+    def Q(self):
+        return self._Q
+
+    @property
+    def p(self):
+        return self._p
+
+    @property
+    def A(self):
+        return self._A
+
+    @property
+    def G(self):
+        return self._G
+
+    @property
+    def h(self):
+        return self._h
+
+    @property
+    def X(self):
+        return self._X
+
+    @property
+    def Y(self):
+        return self._Y
+
+    @property
+    def partial_vars(self):
+        return self._partial_vars
+
+    @property
+    def other_vars(self):
+        return self._other_vars
+
+    @property
+    def partial_unknown_vars(self):
+        return self._partial_vars
+
+    @property
+    def Q_np(self):
+        return self.Q.detach().cpu().numpy()
+
+    @property
+    def p_np(self):
+        return self.p.detach().cpu().numpy()
+
+    @property
+    def A_np(self):
+        return self.A.detach().cpu().numpy()
+
+    @property
+    def G_np(self):
+        return self.G.detach().cpu().numpy()
+
+    @property
+    def h_np(self):
+        return self.h.detach().cpu().numpy()
+
+    @property
+    def X_np(self):
+        return self.X.detach().cpu().numpy()
+
+    @property
+    def Y_np(self):
+        return self.Y.detach().cpu().numpy()
+
+    @property
+    def xdim(self):
+        return self._xdim
+
+    @property
+    def ydim(self):
+        return self._ydim
+
+    @property
+    def num(self):
+        return self._num
+
+    @property
+    def neq(self):
+        return self._neq
+
+    @property
+    def nineq(self):
+        return self._nineq
+
+    @property
+    def nknowns(self):
+        return self._nknowns
+
+    @property
+    def valid_frac(self):
+        return self._valid_frac
+
+    @property
+    def test_frac(self):
+        return self._test_frac
+
+    @property
+    def train_frac(self):
+        return 1 - self.valid_frac - self.test_frac
+
+    @property
+    def trainX(self):
+        return self.X[:int(self.num*self.train_frac)]
+
+    @property
+    def validX(self):
+        return self.X[int(self.num*self.train_frac):int(self.num*(self.train_frac + self.valid_frac))]
+
+    @property
+    def testX(self):
+        return self.X[int(self.num*(self.train_frac + self.valid_frac)):]
+
+    @property
+    def trainY(self):
+        return self.Y[:int(self.num*self.train_frac)]
+
+    @property
+    def validY(self):
+        return self.Y[int(self.num*self.train_frac):int(self.num*(self.train_frac + self.valid_frac))]
+
+    @property
+    def testY(self):
+        return self.Y[int(self.num*(self.train_frac + self.valid_frac)):]
+
+    @property
+    def device(self):
+        return self._device
+
+    def obj_fn(self, Y):
+        return (0.5*(Y@self.Q)*Y + self.p*Y).sum(dim=1)
+
+    def eq_resid(self, X, Y):
+        return X - Y@self.A.T
+
+    def ineq_resid(self, X, Y):
+        return Y@self.G.T - self.h
+
+    def ineq_dist(self, X, Y):
+        resids = self.ineq_resid(X, Y)
+        return torch.clamp(resids, 0)
+
+    def eq_grad(self, X, Y):
+        return 2*(Y@self.A.T - X)@self.A
+
+    def ineq_grad(self, X, Y):
+        ineq_dist = self.ineq_dist(X, Y)
+        return 2*ineq_dist@self.G
+
+    def ineq_partial_grad(self, X, Y):
+        G_effective = self.G[:, self.partial_vars] - self.G[:, self.other_vars] @ (self._A_other_inv @ self._A_partial)
+        h_effective = self.h - (X @ self._A_other_inv.T) @ self.G[:, self.other_vars].T
+        grad = 2 * torch.clamp(Y[:, self.partial_vars] @ G_effective.T - h_effective, 0) @ G_effective
+        Y = torch.zeros(X.shape[0], self.ydim, device=self.device)
+        Y[:, self.partial_vars] = grad
+        Y[:, self.other_vars] = - (grad @ self._A_partial.T) @ self._A_other_inv.T
+        return Y
+
+    # Processes intermediate neural network output
+    def process_output(self, X, Y):
+        return Y
+
+    # Solves for the full set of variables
+    def complete_partial(self, X, Z):
+        Y = torch.zeros(X.shape[0], self.ydim, device=self.device)
+        Y[:, self.partial_vars] = Z
+        Y[:, self.other_vars] = (X - Z @ self._A_partial.T) @ self._A_other_inv.T
+        return Y
+
+    def opt_solve(self, X, solver_type='osqp', tol=1e-4):
+
+        if solver_type == 'qpth':
+            print('running qpth')
+            start_time = time.time()
+            res = QPFunction(eps=tol, verbose=False)(self.Q, self.p, self.G, self.h, self.A, X)
+            end_time = time.time()
+
+            sols = np.array(res.detach().cpu().numpy())
+            total_time = end_time - start_time
+            parallel_time = total_time
+        
+        elif solver_type == 'osqp':
+            print('running osqp')
+            Q, p, A, G, h = \
+                self.Q_np, self.p_np, self.A_np, self.G_np, self.h_np
+            X_np = X.detach().cpu().numpy()
+            Y = []
+            total_time = 0
+            for Xi in X_np:
+                solver = osqp.OSQP()
+                my_A = np.vstack([A, G])
+                my_l = np.hstack([Xi, -np.ones(h.shape[0]) * np.inf])
+                my_u = np.hstack([Xi, h])
+                solver.setup(P=csc_matrix(Q), q=p, A=csc_matrix(my_A), l=my_l, u=my_u, verbose=False, eps_prim_inf=tol)
+                start_time = time.time()
+                results = solver.solve()
+                end_time = time.time()
+
+                total_time += (end_time - start_time)
+                if results.info.status == 'solved':
+                    Y.append(results.x)
+                else:
+                    Y.append(np.ones(self.ydim) * np.nan)
+
+            sols = np.array(Y)
+            parallel_time = total_time/len(X_np)
+
+        else:
+            raise NotImplementedError
+
+        return sols, total_time, parallel_time
+
+    def calc_Y(self):
+        Y = self.opt_solve(self.X)[0]
+        feas_mask =  ~np.isnan(Y).all(axis=1)  
+        self._num = feas_mask.sum()
+        self._X = self._X[feas_mask]
+        self._Y = torch.tensor(Y[feas_mask])
+        return Y
 
